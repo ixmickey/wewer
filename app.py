afcf5d02f784bfbbacb37ba38ce628fbbe615890
ixmickey <52618865+ixmickey@users.noreply.github.com>
2026-03-13 23:26:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,4 +119,8 @@ def status(request_id):
     conn.close()
     return {"status": result[0]}
 
-app.run(debug=True)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+
+app.run(host="0.0.0.0", port=port)
